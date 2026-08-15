c0c4208ff7ac0e891c4d1f7140697d2f21ecaddf
Chandrasekhar-32 <guntreddichandrasekhar2005@gmail.com>
2026-08-16 01:13:02 +0530

    Time: 14 ms (89.93%), Space: 43.4 MB (79.71%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,70 +1,39 @@
 class Solution {
-
     public String longestPalindrome(String s) {
-
-        if (s == null || s.length() == 0) {
-            return "";
+        if (s.length() < 2) {
+            return s;
         }
 
-        // Transform string
-        StringBuilder t = new StringBuilder();
+        int start = 0;
+        int end = 0;
 
-        t.append("#");
+        for (int i = 0; i < s.length(); i++) {
 
-        for (char c : s.toCharArray()) {
-            t.append(c);
-            t.append("#");
-        }
+            // Odd-length palindrome
+            int len1 = expand(s, i, i);
 
-        int n = t.length();
+            // Even-length palindrome
+            int len2 = expand(s, i, i + 1);
 
-        int[] p = new int[n];
+            int len = Math.max(len1, len2);
 
-        int center = 0;
-        int right = 0;
-
-        int maxLen = 0;
-        int centerIndex = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            int mirror = 2 * center - i;
-
-            // Use previously computed palindrome
-            if (i < right) {
-                p[i] = Math.min(right - i, p[mirror]);
-            }
-
-            // Expand around center
-            int left = i - (1 + p[i]);
-            int r = i + (1 + p[i]);
-
-            while (
-                left >= 0 &&
-                r < n &&
-                t.charAt(left) == t.charAt(r)
-            ) {
-                p[i]++;
-                left--;
-                r++;
-            }
-
-            // Update center and right boundary
-            if (i + p[i] > right) {
-                center = i;
-                right = i + p[i];
-            }
-
-            // Find maximum
-            if (p[i] > maxLen) {
-                maxLen = p[i];
-                centerIndex = i;
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
 
-        // Extract answer
-        int start = (centerIndex - maxLen) / 2;
+        return s.substring(start, end + 1);
+    }
 
-        return s.substring(start, start + maxLen);
+    private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length()
+                && s.charAt(left) == s.charAt(right)) {
+
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
     }
 }
