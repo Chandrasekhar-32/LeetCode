61e1c855df26c6c980ff986d177cbabe97b63959
Chandrasekhar-32 <guntreddichandrasekhar2005@gmail.com>
2026-08-20 07:55:46 +0530

    Time: 1 ms (98.08%), Space: 46.9 MB (12.74%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -2,28 +2,30 @@ class Solution {
     public int[] resultArray(int[] nums) {
         int n = nums.length;
 
-        ArrayList<Integer> a = new ArrayList<>();
-        ArrayList<Integer> b = new ArrayList<>();
+        int[] a = new int[n];
+        int[] b = new int[n];
 
-        a.add(nums[0]);
-        b.add(nums[1]);
+        int i = 0, j = 0;
 
-        for (int i = 2; i < n; i++) {
-            if (a.get(a.size() - 1) > b.get(b.size() - 1)) {
-                a.add(nums[i]);
+        a[i++] = nums[0];
+        b[j++] = nums[1];
+
+        for (int k = 2; k < n; k++) {
+            if (a[i - 1] > b[j - 1]) {
+                a[i++] = nums[k];
             } else {
-                b.add(nums[i]);
+                b[j++] = nums[k];
             }
         }
 
         int index = 0;
 
-        for (int x : a) {
-            nums[index++] = x;
+        for (int k = 0; k < i; k++) {
+            nums[index++] = a[k];
         }
 
-        for (int x : b) {
-            nums[index++] = x;
+        for (int k = 0; k < j; k++) {
+            nums[index++] = b[k];
         }
 
         return nums;
